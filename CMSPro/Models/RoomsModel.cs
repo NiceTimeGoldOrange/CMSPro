849fc5d2daae0d84f96258a2bac4df4c91204860
NTGOLjq <1392889987@qq.com>
2019-01-11 16:18:41 +0800

    1.11

---
+++ CMSPro/Models/RoomsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSPro.Models
+{
+    public class RoomsModel
+    {
+        public int Id { get; set; }
+        public string User_Name { get; set; }
+        public string Start_Time { get; set; }
+        public string End_Time { get; set; }
+        public string Meeting_Subject { get; set; }
+        public string Meeting_Title { get; set; }
+        public string Meeting_Remark { get; set; }
+    }
+}
